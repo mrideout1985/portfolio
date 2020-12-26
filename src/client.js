5305af21ae2073e1bd7b0d24887aebdcc53320a2
mrideout1985 <mrideout1985@gmail.com>
2020-12-26 01:44:54 +0000

    created client file to link up with sanityClient

--- src/client.js
+++ src/client.js
@@ -1,1 +1,7 @@
-// talks to santiy studio. What data
+import sanityClient from "@sanity/client";
+// talks to santiy studio. When you grab data - what project you need and where to look etc.
+
+export default sanityClient({
+	projectId: "5q9uij05",
+	dataset: "production",
+});
